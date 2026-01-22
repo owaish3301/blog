e0c71cac4962c2024da33702019016b3fef303ae
Owaish <owaish3301@gmail.com>
2026-01-22 22:44:07 +0530

    feat:added signup component

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import logo from "/logo.svg";
 
 export default function Auth() {
   return (
-    <div>
-        <h2>sdfs</h2>
+    <div className="pt-4 md:pt-12 px-8 max-w-xl m-auto">
+        <div className="w-max m-auto mb-4">
+            <img src={logo} alt="logo" className="m-auto" />
+            <p className="mt-4 text-2xl">Blogify</p>
+        </div>
         <Outlet />
     </div>
   )
