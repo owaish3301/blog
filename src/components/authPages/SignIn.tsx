33532f0f58ea6c92c8b1628c53631a69605a12a7
Owaish <owaish3301@gmail.com>
2026-01-24 18:46:21 +0530

    feat: added signin page

--- src/components/authPages/SignIn.tsx
+++ src/components/authPages/SignIn.tsx
@@ -1,6 +1,84 @@
+import type { FormEvent } from "react";
+import { Link } from "react-router";
+
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Button } from "../../components/ui/button";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "../../components/ui/field";
+import { Input } from "../../components/ui/input";
 
 export default function SignIn() {
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
-    <div>SignIn</div>
-  )
+    <div>
+      <Card className="py-4 sm:py-6 gap-3 sm:gap-6">
+        <CardHeader>
+          <CardTitle>
+            <h1 className="text-2xl font-bold text-center">
+              Welcome back
+            </h1>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={submitHandler}>
+            <FieldGroup>
+              <Field>
+                <Button
+                  variant={"secondary"}
+                  type="button"
+                  className="py-3 h-auto rounded"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path
+                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                  <span>Continue with Google</span>
+                </Button>
+              </Field>
+
+              <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+                Or
+              </FieldSeparator>
+
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="johndoe@example.com"
+                  required
+                />
+
+                <Field>
+                    <FieldLabel htmlFor="password">Password</FieldLabel>
+                    <Input id="password" type="password" required />
+                </Field>
+                <Field>
+                  <Button type="submit">Create Account</Button>
+                </Field>
+
+                <FieldDescription className="text-center">
+                  Don't have an account yet? <Link to="/signup">Sign up</Link>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+      <FieldDescription className="px-6 text-center">
+        By clicking continue, you agree to our{" "}
+        <Link to="#">Terms of Service</Link> and{" "}
+        <Link to="#">Privacy Policy</Link>.
+      </FieldDescription>
+    </div>
+  );
 }
